e6cc4658836169a1060ef6ff9a0ffeae241b8077
Henry7090 <148785054+Henry7090@users.noreply.github.com>
2023-10-24 00:01:31 +0800

    Delete 4e.sql

--- 4e.sql
+++
@@ -1,6 +0,0 @@
-select colors.name, count(distinct inventory_parts.part_num) as unique_parts
-from inventory_parts, colors
-where colors.id = inventory_parts.color_id
-group by colors.id
-order by unique_parts desc
-limit 10
